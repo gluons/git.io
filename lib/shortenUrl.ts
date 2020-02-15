4f5002fdc71d7360c53c27f1940339963082de1c
Saran Tanpituckpong <sarunta@gmail.com>
2020-02-15 20:50:46 +0700

    :sparkles: Use proxy for shortening url

--- lib/shortenUrl.ts
+++ lib/shortenUrl.ts
@@ -1,29 +1,36 @@
 import axios from 'axios';
 
+type Payload = {
+	url: string;
+	code?: string;
+};
+type Result = {
+	shortUrl: string;
+};
+
+const proxyUrl = 'https://git-io-proxy.herokuapp.com/';
+
 export default async function shortenUrl(url: string, code?: string): Promise<string> {
 	if (!url) {
 		throw new Error('No URL to shorten.');
 	}
 
-	const params = new URLSearchParams();
-
-	params.append('url', url);
+	const payload: Payload = {
+		url
+	};
 
 	if (code) {
-		params.append('code', code);
+		payload.code = code;
 	}
 
-	const res = await axios.post('https://git.io', params, {
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded'
-		}
-	});
+	const res = await axios.post<Result | string>(proxyUrl, payload);
+	const { data, status } = res;
 
-	if (res.status !== 201) {
-		throw new Error(`${res.data}`);
+	if (status !== 200) {
+		throw new Error(data.toString());
 	}
 
-	const shortUrl: string = res.headers['location'];
+	const { shortUrl } = data as Result;
 
 	return shortUrl;
 }
